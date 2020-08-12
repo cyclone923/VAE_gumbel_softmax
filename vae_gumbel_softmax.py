0c13c368d6f0f09cc8e1bd73b265441d5e47100b
Chengxi Yang <yangchen@oregonstate.edu>
2020-08-11 22:32:03 -0700

    create my gumbel softmax

--- vae_gumbel_softmax.py
+++ vae_gumbel_softmax.py
@@ -28,6 +28,10 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
+if args.cuda:
+    print("Using GPU")
+else:
+    print("Using CPU")
 
 torch.manual_seed(args.seed)
 if args.cuda:
