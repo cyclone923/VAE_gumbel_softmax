727650e6b4be803af54d04e25efc4632f2143bcd
Chengxi Yang <yangchen@oregonstate.edu>
2020-08-11 23:05:17 -0700

    create my gumbel softmax

--- vae_gumbel_softmax.py
+++ vae_gumbel_softmax.py
@@ -15,7 +15,7 @@ from torchvision.utils import save_image
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=256, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=20, metavar='N',
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--temp', type=float, default=1.0, metavar='S',
                     help='tau(temperature) (default: 1.0)')
