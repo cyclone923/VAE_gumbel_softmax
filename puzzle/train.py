27a1d0cd83c1f61199185de3722cfea1e775c0c2
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-06 03:18:20 -0800

    finish fosae

--- puzzle/train.py
+++ puzzle/train.py
@@ -19,6 +19,7 @@ ANNEAL_RATE = 0.03
 TRAIN_BZ = 2000
 TEST_BZ = 2000
 ALPHA = 0.7
+BETA = 1
 LATENT_DIM_SQRT = int(np.sqrt(LATENT_DIM))
 N_ACTION_SQTR = int(np.sqrt(N_ACTION))
 
@@ -42,7 +43,7 @@ def total_loss(output, o1, o2):
     spasity = 0
     image_loss += rec_loss_function(recon_o1, o1, nn.BCELoss(reduction='none'))
     image_loss += rec_loss_function(recon_o2, o2, nn.BCELoss(reduction='none'))
-    latent_loss += rec_loss_function(recon_z2, z2.detach(), nn.BCELoss(reduction='none'))
+    latent_loss += rec_loss_function(recon_z2, z2, nn.MSELoss(reduction='none')) * BETA
     spasity += latent_spasity(z1)
     spasity += latent_spasity(z2)
     return image_loss, latent_loss, spasity
@@ -153,7 +154,7 @@ def run(n_epoch):
         temp1 = np.maximum(TEMP_BEGIN_SAE * np.exp(-ANNEAL_RATE * e), TEMP_MIN_SAE)
         temp2 = np.maximum(TEMP_BEGIN_AAE * np.exp(-ANNEAL_RATE * e), TEMP_MIN_AAE)
         print("Epoch: {}, Temperature: {:.2f} {:.2f}, Lr: {}".format(e, temp1, temp2, scheculer.get_last_lr()))
-        train_loss = train(train_loader, vae, optimizer, (temp1, temp2), e >= 100)
+        train_loss = train(train_loader, vae, optimizer, (temp1, temp2), e >= 0)
         print('====> Epoch: {} Average train loss: {:.4f}'.format(e, train_loss))
         test_loss = test(test_loader, vae, e)
         print('====> Epoch: {} Average test loss: {:.4f}, best loss {:.4f}'.format(e, test_loss, best_loss))
