121b529bf14dac26f0413de4e40ef45689ea5a39
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-07 07:39:28 -0800

    finish fosae

--- puzzle/train.py
+++ puzzle/train.py
@@ -43,7 +43,7 @@ def total_loss(output, o1, o2):
     image_loss += rec_loss_function(recon_o1, o1, nn.BCELoss(reduction='none'))
     image_loss += rec_loss_function(recon_o2, o2, nn.BCELoss(reduction='none'))
     image_loss += rec_loss_function(recon_o2_tilda, o2, nn.BCELoss(reduction='none'))
-    latent_loss += rec_loss_function(recon_z2, z2, nn.L1Loss(reduction='none')) * BETA
+    latent_loss += rec_loss_function(recon_z2, z2.detach(), nn.L1Loss(reduction='none')) * BETA
     spasity += latent_spasity(z1)
     spasity += latent_spasity(z2)
     spasity += latent_spasity(recon_z2)
