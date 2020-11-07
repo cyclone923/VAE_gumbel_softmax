d53701a70b2eb1eff49daa09b69f650d6ee29657
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-06 21:25:37 -0800

    finish fosae

--- puzzle/sae.py
+++ puzzle/sae.py
@@ -53,11 +53,19 @@ class Aae(nn.Module):
     def __init__(self):
         super(Aae, self).__init__()
         self.fc1 = nn.Linear(in_features=LATENT_DIM + LATENT_DIM, out_features=400)
+        self.bn1 = nn.BatchNorm1d(num_features=1)
+        self.dpt1 = nn.Dropout(0.4)
         self.fc2 = nn.Linear(in_features=400 + LATENT_DIM, out_features=400)
+        self.bn2 = nn.BatchNorm1d(num_features=1)
+        self.dpt2 = nn.Dropout(0.4)
         self.fc3 = nn.Linear(in_features=400 + LATENT_DIM, out_features=N_ACTION)
 
         self.fc4 = nn.Linear(in_features=N_ACTION, out_features=400)
+        self.bn4 = nn.BatchNorm1d(num_features=1)
+        self.dpt4 = nn.Dropout(0.4)
         self.fc5 = nn.Linear(in_features=400, out_features=400)
+        self.bn5 = nn.BatchNorm1d(num_features=1)
+        self.dpt5 = nn.Dropout(0.4)
         self.fc6 = nn.Linear(in_features=400, out_features=LATENT_DIM)
 
         self.bn_input = nn.BatchNorm1d(num_features=1)
@@ -67,8 +75,8 @@ class Aae(nn.Module):
     def encode(self, s, z, temp):
         s = torch.flatten(s, start_dim=1).unsqueeze(1)
         z = torch.flatten(z, start_dim=1).unsqueeze(1)
-        h1 = torch.relu(self.fc1(torch.cat([s, z], dim=2)))
-        h2 = torch.relu(self.fc2(torch.cat([s, h1], dim=2)))
+        h1 = bn_and_dpt(torch.relu(self.fc1(torch.cat([s, z], dim=2))), self.bn1, self.dpt1)
+        h2 = bn_and_dpt(torch.relu(self.fc2(torch.cat([s, h1], dim=2))), self.bn2, self.dpt2)
         h3 = self.fc3(torch.cat([s, h2], dim=2))
         return gumbel_softmax(h3.view(-1, 1, N_ACTION), temp)
 
@@ -76,11 +84,11 @@ class Aae(nn.Module):
         s = torch.flatten(s, start_dim=1).unsqueeze(1)
         s = self.bn_input(s)
         s = s.view(-1, LATENT_DIM, 1)
-        h1 = torch.relu(self.fc4(a))
-        h2 = torch.relu(self.fc5(h1))
-        h3 = self.bn_effect(self.fc6(h2))
-        h3 = h3.view(-1, LATENT_DIM, 1)
-        return gumbel_softmax(h3+s, temp)
+        h4 = bn_and_dpt(torch.relu(self.fc4(a)), self.bn4, self.dpt4)
+        h5 = bn_and_dpt(torch.relu(self.fc5(h4)), self.bn5, self.dpt5)
+        h6 = self.bn_effect(self.fc6(h5))
+        h6 = h6.view(-1, LATENT_DIM, 1)
+        return gumbel_softmax(h6+s, temp)
 
     def forward(self, s, z, temp):
         a = self.encode(s, z, temp)
