3dde5fd59453ae9c801fa291349edf78abf48379
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-07 06:38:04 -0800

    finish fosae

--- puzzle/train.py
+++ puzzle/train.py
@@ -167,9 +167,9 @@ def run(n_epoch):
     for e in range(n_epoch):
         temp1 = np.maximum(TEMP_BEGIN_SAE * np.exp(-ANNEAL_RATE * e), TEMP_MIN_SAE)
         temp2 = np.maximum(TEMP_BEGIN_AAE * np.exp(-ANNEAL_RATE * e), TEMP_MIN_AAE)
-        # print("Epoch: {}, Temperature: {:.2f} {:.2f}, Lr: {}".format(e, temp1, temp2, scheculer.get_last_lr()))
-        # train_loss = train(train_loader, vae, optimizer, (temp1, temp2), e >= 100)
-        # print('====> Epoch: {} Average train loss: {:.4f}'.format(e, train_loss))
+        print("Epoch: {}, Temperature: {:.2f} {:.2f}, Lr: {}".format(e, temp1, temp2, scheculer.get_last_lr()))
+        train_loss = train(train_loader, vae, optimizer, (temp1, temp2), e >= 100)
+        print('====> Epoch: {} Average train loss: {:.4f}'.format(e, train_loss))
         test_loss = test(test_loader, vae, e, (temp1, temp2))
         print('====> Epoch: {} Average test loss: {:.4f}, best loss {:.4f} in epoch {}'.format(e, test_loss, best_loss, best_epoch))
         if test_loss < best_loss:
