33ce49145cf22a65b70e1e1124b962bfde64e7a2
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-09 06:38:45 -0800

    finish fosae

--- puzzle/loss.py
+++ puzzle/loss.py
@@ -1,15 +1,15 @@
 import torch.nn as nn
 
-ALPHA = 0.1
-BETA = 0.1
+ALPHA = 0.01
+BETA = 0.05
 
 # Reconstruction + zero suppressed losses summed over all elements and batch
-def rec_loss_function(recon_x, x, criterion):
+def rec_loss_function(recon_x, x, criterion, weight=1.):
     BCE = criterion(recon_x, x).mean()
-    return BCE
+    return BCE * weight
 
-def latent_spasity(z):
-    return z.mean() * ALPHA
+def latent_spasity(z, weight=1.):
+    return z.mean() * weight
 
 def total_loss(output, o1, o2):
     recon_o1, recon_o2, recon_o2_tilde, z1, z2, recon_z2, _, _, _ = output
@@ -19,8 +19,8 @@ def total_loss(output, o1, o2):
     image_loss += rec_loss_function(recon_o1, o1, nn.BCELoss(reduction='none'))
     image_loss += rec_loss_function(recon_o2, o2, nn.BCELoss(reduction='none'))
     image_loss += rec_loss_function(recon_o2_tilde, o2, nn.BCELoss(reduction='none'))
-    latent_loss += rec_loss_function(recon_z2, z2, nn.L1Loss(reduction='none')) * BETA
-    spasity += latent_spasity(z1)
-    spasity += latent_spasity(z2)
-    spasity += latent_spasity(recon_z2)
+    latent_loss += rec_loss_function(recon_z2, z2, nn.L1Loss(reduction='none'), BETA)
+    spasity += latent_spasity(z1, ALPHA)
+    spasity += latent_spasity(z2, ALPHA)
+    spasity += latent_spasity(recon_z2, ALPHA)
     return image_loss, latent_loss, spasity
