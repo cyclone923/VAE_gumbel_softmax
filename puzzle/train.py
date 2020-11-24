706107b9bbb8cfe40cc370600c416474d0c37c6a
Chengxi Yang <yangchen@oregonstate.edu>
2020-11-23 17:48:39 -0800

    merge

--- puzzle/train.py
+++ puzzle/train.py
@@ -157,5 +157,5 @@ if __name__ == "__main__":
     os.makedirs(SAMPLE_DIR, exist_ok=True)
     os.makedirs(SAMPLE_DIR_ARGMAX, exist_ok=True)
     os.makedirs(MODEL_DIR, exist_ok=True)
-    run(1000)
+    run(10)
     to_gif(PLOT_DIR)
